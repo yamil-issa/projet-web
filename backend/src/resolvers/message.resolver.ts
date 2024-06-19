dc28502c90c2b1f7ac5c0e619f2a2e8f60b1f691
Yamil <issayamilrt@gmail.com>
2024-06-19 09:24:11 +0200

    feature: add test

--- backend/src/resolvers/message.resolver.ts
+++ backend/src/resolvers/message.resolver.ts
@@ -1,24 +1,10 @@
-import { Resolver, Query, Args, Int, Mutation } from '@nestjs/graphql';
-import { Message } from 'src/entities/message.entity';
-import { SendMessageMutation } from 'src/mutations/message/sendMessage';
-import { BullQueueProvider } from 'src/infrastructure/bullmq/bullQueue.provider';
+import { Resolver, Query, Args, Int} from '@nestjs/graphql';
+import { Message } from '../entities/message.entity';
+import { messagesArray } from '../data';
 
 @Resolver()
 export class MessageResolver {
-  private messages = [
-        {
-            id: 1,
-            content: 'Hello Jane',
-            createdAt: '2024-05-01T12:00:00Z',
-            author: {
-                id: 1,
-                username: 'john_doe',
-                email: 'doe@gmail.com',
-                password: '123',
-                conversations: [],
-            },
-        },
-    ];
+  private messages = messagesArray;
 
   @Query(() => Message)
   message(@Args('id', { type: () => Int }) id: number): Message | null{
---
+++ backend/src/tests/message.resolver.spec.ts
@@ -0,0 +1,40 @@
+// src/resolvers/message.resolver.spec.ts
+import { Test, TestingModule } from '@nestjs/testing';
+import { MessageResolver } from '../resolvers/message.resolver';
+import { Message } from '../entities/message.entity';
+import { messagesArray } from '../data';
+
+describe('MessageResolver', () => {
+  let resolver: MessageResolver;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [MessageResolver],
+    }).compile();
+
+    resolver = module.get<MessageResolver>(MessageResolver);
+  });
+
+  it('should be defined', () => {
+    expect(resolver).toBeDefined();
+  });
+
+  describe('message', () => {
+    it('should return a message by id', () => {
+      const messageId = 1;
+      const expectedMessage = messagesArray.find((message) => message.id === messageId) as Message;
+    
+      const result = resolver.message(messageId);
+    
+      expect(result).toEqual(expectedMessage);
+    });
+
+    it('should return null if message id does not exist', () => {
+      const messageId = 3;
+
+      const result = resolver.message(messageId);
+
+      expect(result).toBeNull();
+    });
+  });
+});
